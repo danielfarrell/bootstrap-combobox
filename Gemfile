406cf9c303c9f752b7dd18fc00ebd0e1304386a0
denishaskin <denis@constantorbit.com>
2013-07-22 14:01:48 -0400

    Make publishable as a gem. #80.

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in bootstrap-combobox-rails.gemspec
+gemspec
---
+++ lib/bootstrap-combobox.rb
@@ -0,0 +1,11 @@
+require "bootstrap-combobox-rails/version"
+
+module Bootstrap
+  module Combobox
+    module Rails
+      class Engine < ::Rails::Engine
+        # Rails, will you please look in our vendor? kthx
+      end    
+    end
+  end
+end
