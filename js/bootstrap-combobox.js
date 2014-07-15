c9ac71a750e99afa94228428731c2d1b96b73ceb
tim.tune <tim.tune@pssbalpwld01.hc.int>
2014-07-15 12:51:18 +1000

    Fixed issue with not being able to use own template upon initialization

--- js/bootstrap-combobox.js
+++ js/bootstrap-combobox.js
@@ -26,15 +26,15 @@
   var Combobox = function ( element, options ) {
     this.options = $.extend({}, $.fn.combobox.defaults, options);
     this.$source = $(element);
-    this.$container = this.setup();
-    this.$element = this.$container.find('input[type=text]');
-    this.$target = this.$container.find('input[type=hidden]');
-    this.$button = this.$container.find('.dropdown-toggle');
     this.$menu = $(this.options.menu).appendTo('body');
     this.template = this.options.template || this.template
     this.matcher = this.options.matcher || this.matcher;
     this.sorter = this.options.sorter || this.sorter;
     this.highlighter = this.options.highlighter || this.highlighter;
+    this.$container = this.setup();
+    this.$element = this.$container.find('input[type=text]');
+    this.$target = this.$container.find('input[type=hidden]');
+    this.$button = this.$container.find('.dropdown-toggle');
     this.shown = false;
     this.selected = false;
     this.refresh();
